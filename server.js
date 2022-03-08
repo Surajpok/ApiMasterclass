02b04e3f2c642aa7579146b7fa569c7ffb9ba206
suraj <Pokhrel004@gmail.com>
2022-03-08 18:32:28 +0545

    initial update

--- server.js
+++ server.js
@@ -2,25 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv');
 
 const app = express();
-app.get('/api/v1/bootcamps', (req,res) =>{
-    res.status(200).json({success:true,msg:'show all bootcamps'});
-});
 
-app.get('/api/v1/bootcamps/:id', (req,res) =>{
-    res.status(200).json({success:true,msg:`Get Bootcamp ${req.params.id}`});
-});
+// Routes Files
+const bootcamps = require('./routes/bootcamps');
 
-app.post('/api/v1/bootcamps', (req,res) =>{
-    res.status(200).json({success:true,msg:'Create New bootcamps'});
-});
-
-app.put('/api/v1/bootcamps/:id', (req,res) =>{
-    res.status(200).json({success:true,msg:`Update Bootcamp ${req.params.id}`});
-});
-
-app.delete('/api/v1/bootcamps/:id', (req,res) =>{
-    res.status(200).json({success:true,msg:`Delete Bootcamp ${req.params.id}`});
-});
+app.use('/api/v1/bootcamps',bootcamps);
 
 // load env vars
 dotenv.config({path:'./config/config.env'});
